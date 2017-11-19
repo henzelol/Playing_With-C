a9b6a61a345e4bab9f54b25e7365ee80603b1167
Henzel Santos <henzelmsn3002@hotmail.com>
2017-11-18 22:30:24 -0200

    Create Struct_FirstShot.c

---
+++ Struct_FirstShot.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+
+
+struct tcarro {
+
+	char cor[10];
+
+	unsigned short int ano;
+
+	unsigned short int preco;
+
+	char marca[15];
+
+};
+
+
+
+void preencher(struct tcarro v[])	
+
+
+
+int main(){
+
+
+
+struct tcarro v[3];
+
+
+
+preencher(tcarro)
+
+	
+
+}
+
+
+
+void preencher(struct tcarro v[]){
+
+	for (int i=0;i<20;i++){
+
+		printf("digite a cor co carro");
+
+		scanf("%s", v[i].cor);
+
+		printf("digite p ano carro");
+
+		scanf("%d", v[i].ano);
+
+		printf("digite o preco do carro");
+
+		scanf("%d", v[i].preco);
+
+		printf("digite a cor co carro");
+
+		scanf("%s", v[i].marca);
+
+		
+
+		
+
+		fflush(stdin);
+
+	}
+
+}
